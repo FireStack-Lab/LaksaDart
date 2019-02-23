f8e21a33b4d57af9b42e65a8e1a931ce7bf534e7
neeboo <ghostcorn@gmail.com>
2019-02-23 17:50:52 +0800

    modify isolates added correct error handler

--- lib/src/crypto/isolates.dart
+++ lib/src/crypto/isolates.dart
@@ -6,24 +6,31 @@ import 'keystore/api.dart';
 //这里以计算斐波那契数列为例，返回的值是Future，因为是异步的
 Future<dynamic> asyncEncrypt(String prvKey, String psw,
     [Map<String, dynamic> options]) async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
-  //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
-  //_isolate是创建isolate必须要的参数。
-  var isolate = await Isolate.spawn(_isolate_encrypt, response.sendPort,
-      onExit: response.sendPort);
-  //获取sendPort来发送数据
-  final sendPort = await response.first as SendPort;
-  //接收消息的ReceivePort
-  final answer = new ReceivePort();
-  //发送数据
-  sendPort.send([prvKey, psw, options, answer.sendPort]);
-  //获得数据并返回
-  final result = await answer.first;
-  response.close();
 
-  return result;
+  await Isolate.spawn(
+    _isolate_encrypt,
+    response.sendPort,
+    onExit: response.sendPort,
+  );
+
+  final sendPort = await response.first as SendPort;
+  final receivePort = new ReceivePort();
+  //发送数据
+  sendPort.send([prvKey, psw, options, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -33,13 +40,18 @@ void _isolate_encrypt(SendPort initialReplyTo) {
   initialReplyTo.send(port.sendPort);
   //监听
   port.listen((message) async {
-    //获取数据并解析
-    final prvKey = message[0] as String;
-    final psw = message[1] as String;
-    final options = message[2] as Map<String, dynamic>;
-    final send = message[3] as SendPort;
-    //返回结果
-    send.send(await encrypt(prvKey, psw, options));
+    try {
+      final prvKey = message[0] as String;
+      final psw = message[1] as String;
+      final options = message[2] as Map<String, dynamic>;
+      final send = message.last as SendPort;
+      var encrypted;
+      encrypted = await encrypt(prvKey, psw, options);
+      //返回结果
+      send.send(encrypted);
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
   });
 }
 
@@ -48,23 +60,35 @@ Future<dynamic> asyncDecrypt(
   Map<String, dynamic> keyStore,
   String psw,
 ) async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
+  // standard procedure
   final response = new ReceivePort();
-  //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
-  //_isolate是创建isolate必须要的参数。
-  await Isolate.spawn(_isolate_decrypt, response.sendPort,
-      onExit: response.sendPort);
-  //获取sendPort来发送数据
+
+  await Isolate.spawn(
+    _isolate_decrypt,
+    response.sendPort,
+    onExit: response.sendPort,
+    onError: response.sendPort,
+  );
+
+  // setup sendport and receiveport;
+
   final sendPort = await response.first as SendPort;
-  //接收消息的ReceivePort
-  final answer = new ReceivePort();
-  //发送数据
-  sendPort.send([keyStore, psw, answer.sendPort]);
-  final result = await answer.first;
-  //获得数据并返回
-  response.close();
-  return result;
+  final receivePort = new ReceivePort();
+
+  sendPort.send([keyStore, psw, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -73,36 +97,53 @@ void _isolate_decrypt(SendPort initialReplyTo) {
   //绑定
   initialReplyTo.send(port.sendPort);
   //监听
-  port.listen((message) async {
-    //获取数据并解析
-    final keyStore = message[0] as Map<String, dynamic>;
-    final psw = message[1] as String;
-    final send = message[2] as SendPort;
-    //返回结果
 
-    send.send(await decrypt(keyStore, psw));
+  port.listen((message) async {
+    try {
+      final send = message.last as SendPort;
+      final keyStore = message[0] as Map<String, dynamic>;
+      final psw = message[1] as String;
+      //返回结果
+      var decrypted = await decrypt(keyStore, psw);
+      send.send(decrypted);
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
   });
 }
 
 //这里以计算斐波那契数列为例，返回的值是Future，因为是异步的
 Future<dynamic> asyncGeneratePrivateKey() async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
+
   //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
   //_isolate是创建isolate必须要的参数。
   await Isolate.spawn(_isolate_genratePrvKey, response.sendPort,
       onExit: response.sendPort);
+
+  // setup error listener
+  // should place before normal result
+
   //获取sendPort来发送数据
   final sendPort = await response.first as SendPort;
   //接收消息的ReceivePort
-  final answer = new ReceivePort();
+  final receivePort = new ReceivePort();
   //发送数据
-  sendPort.send([answer.sendPort]);
-  final result = await answer.first;
-  //获得数据并返回
-  response.close();
-  return result;
+
+  sendPort.send([receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -113,10 +154,13 @@ void _isolate_genratePrvKey(SendPort initialReplyTo) {
   //监听
   port.listen((message) {
     //获取数据并解析
-
-    final send = message[0] as SendPort;
-    //返回结果
-    send.send(generatePrivateKey());
+    try {
+      final send = message.last as SendPort;
+      //返回结果
+      send.send(generatePrivateKey());
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
   });
 }
 
@@ -124,23 +168,32 @@ Future<dynamic> asyncSchnorrSign(
   String privateKey,
   Map<String, dynamic> txnDetails,
 ) async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
-  //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
-  //_isolate是创建isolate必须要的参数。
-  await Isolate.spawn(_isolate_SchnorrSign, response.sendPort,
-      onExit: response.sendPort);
+
+  await Isolate.spawn(
+    _isolate_SchnorrSign,
+    response.sendPort,
+    onExit: response.sendPort,
+  );
   //获取sendPort来发送数据
   final sendPort = await response.first as SendPort;
   //接收消息的ReceivePort
-  final answer = new ReceivePort();
+  final receivePort = new ReceivePort();
   //发送数据
-  sendPort.send([privateKey, txnDetails, answer.sendPort]);
-  //获得数据并返回
-  final result = answer.first;
-  response.close();
-  return result;
+  sendPort.send([privateKey, txnDetails, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -151,34 +204,49 @@ void _isolate_SchnorrSign(SendPort initialReplyTo) {
   //监听
   port.listen((message) {
     //获取数据并解析
-    final prv = message[0] as String;
-    final txn = message[1] as Map<String, dynamic>;
-    final send = message[2] as SendPort;
-    //返回结果
-    send.send(SchnorrSign(prv, txn));
+    try {
+      final prv = message[0] as String;
+      final txn = message[1] as Map<String, dynamic>;
+      final send = message.last as SendPort;
+      //返回结果
+      send.send(SchnorrSign(prv, txn));
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
   });
 }
 
 Future<dynamic> asyncGetPubKeyFromPrivateKey(
   String privateKey,
 ) async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
+
   //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
   //_isolate是创建isolate必须要的参数。
-  final isolate =
-      await Isolate.spawn(_isolate_getPubKeyFromPrivateKey, response.sendPort);
+
+  await Isolate.spawn(
+    _isolate_getPubKeyFromPrivateKey,
+    response.sendPort,
+  );
   //获取sendPort来发送数据
   final sendPort = await response.first as SendPort;
   //接收消息的ReceivePort
-  final answer = new ReceivePort();
+  final receivePort = new ReceivePort();
   //发送数据
-  sendPort.send([privateKey, answer.sendPort]);
-  final result = answer.first;
-  response.close();
-  //获得数据并返回
-  return result;
+  sendPort.send([privateKey, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -188,70 +256,93 @@ void _isolate_getPubKeyFromPrivateKey(SendPort initialReplyTo) {
   initialReplyTo.send(port.sendPort);
   //监听
   port.listen((message) {
+    try {
+      final prv = message[0] as String;
+      final send = message.last as SendPort;
+      //返回结果
+      send.send(getPubKeyFromPrivateKey(prv));
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
     //获取数据并解析
-    final prv = message[0] as String;
-    final send = message[1] as SendPort;
-    //返回结果
-    send.send(getPubKeyFromPrivateKey(prv));
   });
 }
 
 Future<dynamic> asyncGetAddressFromPrivateKey(
   String privateKey,
 ) async {
-  //首先创建一个ReceivePort，为什么要创建这个？
-  //因为创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
-  //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
-  //_isolate是创建isolate必须要的参数。
-  await Isolate.spawn(_isolate_getAddressFromPrivateKey, response.sendPort);
-  //获取sendPort来发送数据
+
+  await Isolate.spawn(
+    _isolate_getAddressFromPrivateKey,
+    response.sendPort,
+  );
+
   final sendPort = await response.first as SendPort;
-  //接收消息的ReceivePort
-  final answer = new ReceivePort();
-  //发送数据
-  sendPort.send([privateKey, answer.sendPort]);
-  final result = answer.first;
-  response.close();
-  //获得数据并返回
-  return result;
+
+  final receivePort = new ReceivePort();
+
+  sendPort.send([privateKey, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
-//创建isolate必须要的参数
 void _isolate_getAddressFromPrivateKey(SendPort initialReplyTo) {
   final port = new ReceivePort();
   //绑定
   initialReplyTo.send(port.sendPort);
   //监听
   port.listen((message) {
-    //获取���据并解析
-    final prv = message[0] as String;
-    final send = message[1] as SendPort;
-    //返回结果
-    send.send(getAddressFromPrivateKey(prv));
+    try {
+      final prv = message[0] as String;
+      final send = message.last as SendPort;
+      //返回结果
+      send.send(getAddressFromPrivateKey(prv));
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
   });
 }
 
 Future<dynamic> asyncGetAddressFromPublicKey(
   String pubKey,
 ) async {
-  //��先创建一个ReceivePort，为什么要创建这个？
-  //因���创建isolate所需的参数，必须要有SendPort，SendPort需要ReceivePort来创建
   final response = new ReceivePort();
-  //开始创建isolate,Isolate.spawn函数是isolate.dart里的代码,_isolate是我们自己实现的函数
-  //_isolate是创建isolate必须要的参数。
-  await Isolate.spawn(_isolate_getAddressFromPublicKey, response.sendPort);
-  //获取sendPort来发送数据
+
+  await Isolate.spawn(
+    _isolate_getAddressFromPublicKey,
+    response.sendPort,
+  );
+
   final sendPort = await response.first as SendPort;
-  //接收消息的ReceivePort
-  final answer = new ReceivePort();
-  //发送数据
-  sendPort.send([pubKey, answer.sendPort]);
-  //获得数据并返回
-  final result = answer.first;
-  response.close();
-  //获得数据并返回
-  return result;
+
+  final receivePort = new ReceivePort();
+
+  sendPort.send([pubKey, receivePort.sendPort]);
+
+  try {
+    final result = await receivePort.first;
+    var resultString = result.toString();
+
+    if (resultString.startsWith('@@LaksaError@@')) {
+      throw resultString.substring(14);
+    }
+    response.close();
+    return result;
+  } catch (e) {
+    throw e;
+  }
 }
 
 //创建isolate必须要的参数
@@ -262,10 +353,78 @@ void _isolate_getAddressFromPublicKey(SendPort initialReplyTo) {
   //监听
   port.listen((message) {
     //获取数据并解析
-    final pub = message[0] as String;
+    try {
+      final pub = message[0] as String;
+      final send = message.last as SendPort;
+      //返回结果
+      var signed = getAddressFromPublicKey(pub);
+      send.send(signed);
+    } catch (e) {
+      message.last.send('@@LaksaError@@$e');
+    }
+  });
+}
+
+class IsolateFunction {
+  List<dynamic> params;
+  Function func;
+
+  final response = new ReceivePort();
+  final receivePort = new ReceivePort();
+  Isolate _isolate_instance;
+
+  IsolateFunction({this.params, this.func});
+
+  run() async {
+    _isolate_instance = await Isolate.spawn(
+        _isolate_function, response.sendPort,
+        onExit: response.sendPort);
+    final sendPort = await response.first as SendPort;
+
+    sendPort.send([
+      this.func,
+      receivePort.sendPort,
+      this.params.iterator,
+    ]);
+    try {
+      final result = await receivePort.first;
+      var resultString = result.toString();
+
+      if (resultString.startsWith('@@LaksaError@@')) {
+        throw resultString.substring(14);
+      }
+      // response.close();
+      kill();
+      return result;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  void kill() {
+    if (_isolate_instance != null) {
+      _isolate_instance.kill();
+      response.close();
+      receivePort.close();
+    }
+  }
+}
+
+void _isolate_function(SendPort initialReplyTo) {
+  final port = new ReceivePort();
+
+  initialReplyTo.send(port.sendPort);
+
+  port.listen((message) async {
+    final func = message[0];
     final send = message[1] as SendPort;
-    //返回结果
-    var signed = getAddressFromPublicKey(pub);
-    send.send(signed);
+    try {
+      final sendParams = message.removeRange(0, 2);
+      //返回结果
+      var result = await func(sendParams);
+      send.send(result);
+    } catch (e) {
+      send.send('@@LaksaError@@$e');
+    }
   });
 }
